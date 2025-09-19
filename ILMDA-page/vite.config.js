161f407dea9715518179d3db841a910302cc65fe
jose Valadez <josevala04@gmail.com>
2025-09-19 13:34:09 -0700

    deployment attempt

--- ILMDA-page/vite.config.js
+++ ILMDA-page/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DLX7-ILMDA/', // <-- add this line for GitHub Pages subfolder
   plugins: [
     vue(),
     vueDevTools(),
